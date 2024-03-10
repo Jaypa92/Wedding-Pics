e68f74f42ca0ea0c7d7be5f1b130db5ba99e9a0a
[Jaypa92] <j.parris1@aol.com>
2024-03-10 00:39:53 -0500

    commit

---
+++ client/src/components/Grid.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Grid = ({photos}) => {
+    return (
+        <>
+        <h1>Our Gallery</h1>
+        <div className="grid">
+            {photos.map(({photo, _id}) => (
+                <div key={_id} className='grid_item'>
+                    <img 
+                    src={`http://localhost:5000/uploads/${photo}`} 
+                    alt="grid_image" />
+                </div>
+            ))}
+        </div>
+        </>
+    )
+}
+
+export default Grid
