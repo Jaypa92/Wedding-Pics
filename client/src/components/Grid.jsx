04b13e0809d4e2269be13971d3711cb5f571a29d
[Jaypa92] <j.parris1@aol.com>
2024-03-10 19:37:33 -0400

    commit

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 const Grid = ({photos}) => {
+
+    const navigate = useNavigate();
 
     const handleDelete = (e, id) => {
         e.preventDefault();
         axios.delete(`http://localhost:5000/api/delete/${id}`)
             .then(res=>{
                 console.log(res);
-            })
+                navigate('/');
+            });
     }
 
     return (
@@ -16,8 +20,8 @@ const Grid = ({photos}) => {
         <h1>Our Gallery</h1>
         <div className="grid">
             {photos.map(({photo, _id}) => (
-                <div className='box'>
-                    <div key={_id} className='grid_item'>
+                <div key={_id} className='box'>
+                    <div className='grid_item'>
                         <img 
                         src={`http://localhost:5000/uploads/${photo}`} 
                         alt="grid_image" />
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser');
 const uri = "mongodb+srv://Jaypa92:Iamjaypa92@cluster0.ivw7cmn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;"
 require("dotenv").config()
 
@@ -8,8 +9,10 @@ const UploadRoute = require('./routes/uploadRoute');
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 5000;
@@ -31,3 +34,21 @@ app.use(UploadRoute);
 app.listen(PORT, () => {
     console.log(`Server started at port: ${PORT}`);
 })
+
+const User = mongoose.model('User', {
+    userId: String,
+    lastVisit: Date,
+});
+
+app.use(async (req, res, next) => {
+    const userId = req.cookies.userId || createUserId();
+
+    await User.findOneAndUpdate({userId}, {lastVisit: new Date()}, {upsert: true} );
+    res.cookie('userId', userId, {maxAge: 900000, httpOnly: true});
+
+    next();
+});
+
+function createUserId(){
+    return Math.random().toString(36).substring(7);
+}
