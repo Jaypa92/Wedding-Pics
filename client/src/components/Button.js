79fab1e55aae12a3dc93a3675f66c26142ec5d2e
[Jaypa92] <j.parris1@aol.com>
2024-03-24 18:18:41 -0400

    commit

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,32 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {AiFillPlusCircle} from 'react-icons/ai';
 import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
 
 const Button = ({setUpdateUI}) => {
+    const navigate = useNavigate();
+    const [selectedFiles, setSelectedFiles] = useState([]);
 
     const handleChange = (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-
-        formData.append("photo", e.target.files[0])
-        
-        axios.post("http://localhost:5000/api/save", formData)
-            .then((res) => {
-                console.log(res.data);
-                setUpdateUI(res.data._id);
-            })
-            .catch((err) => console.log(err));
+        const files = e.target.files;
+        setSelectedFiles([...selectedFiles, ...files]);
     }
 
-    return <label className='button' htmlFor='file_picker'>
-        <AiFillPlusCircle/>
-        <input 
-            hidden 
-            type='file' 
-            name='file_picker' 
-            id='file_picker' 
-            onChange={(e) => handleChange(e)} />
-    </label>;
+    useEffect(() => {
+        
+    }, [selectedFiles]);
+
+    const handleUpload = async () => {
+        
+        const formData = new FormData();
+
+        selectedFiles.forEach((file) => {
+            formData.append('photo', file);
+            console.log(selectedFiles.length)
+        })
+
+        try {
+            const response = await axios.post('http://localhost:5000/api/save', formData)
+            console.log(response.data);
+            setUpdateUI(response.data._id);
+            navigate('/');
+        } catch (error) {
+            console.error('Error uploading files', error);
+        }
+        }
+
+    return (
+    <div>
+        <label className='button' htmlFor='file_picker'>
+            <AiFillPlusCircle/>
+            <input 
+                hidden 
+                multiple
+                type='file' 
+                name='file_picker' 
+                id='file_picker' 
+                onChange={handleChange} />
+        </label>
+        <button onClick={handleUpload}>Upload</button>
+    </div>
+    )
 }
 
 export default Button
