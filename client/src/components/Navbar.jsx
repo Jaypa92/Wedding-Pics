f2aeda26d0ed0d7d09063143da75200b0a92e070
[Jaypa92] <j.parris1@aol.com>
2024-05-05 16:15:59 -0400

    commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
 const Navbar = () => {
+
+    document.addEventListener("DOMContentLoaded", function(){
+        var scroll = document.getElementById('scroll');
+
+        scroll.addEventListener('click', function() {
+            window.scrollTo({
+                top:0,
+                behavior:"smooth"
+            })
+        })
+    })
+
     return (
     <nav>
         <div className="nav-logo">
             Parris Family Wedding 
         </div>
+        <a href="#" id='scroll'>Back To Top</a>
     </nav>
     )
 }
