584f24ec516280948f9d412a234c19476417d647
[Jaypa92] <j.parris1@aol.com>
2025-01-12 00:51:01 -0500

    commit

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
 
     useEffect(() => {
 
-        axios.get('http://parriswedding.com/api/get')
+        axios.get('https://parriswedding.com/api/get')
             .then((res) => {
                 console.log(res.data);
                 setPhotos(res.data);
