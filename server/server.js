acf72ba560e38bcb4fc343364e31954eafa2bc98
[Jaypa92] <j.parris1@aol.com>
2025-01-13 19:40:18 -0500

    commit

--- server/server.js
+++ server/server.js
@@ -18,18 +18,18 @@ const PORT = process.env.PORT || 5000;
 
 app.use(UploadRoute);
 app.use('/uploads', express.static(path.join(__dirname, '/public/uploads')));
-app.use(express.static(path.join(__dirname, '../client/build')))
+// app.use(express.static(path.join(__dirname, '../client/build')))
 
-app.get("*", (req, res) => {
-    res.sendFile(
-        path.join(__dirname, "../client/build","index.html"),
-        function (err) {
-            if (err) {
-                res.status(500).send(err);
-            }
-        }
-    )
-})
+// app.get("*", (req, res) => {
+//     res.sendFile(
+//         path.join(__dirname, "../client/build","index.html"),
+//         function (err) {
+//             if (err) {
+//                 res.status(500).send(err);
+//             }
+//         }
+//     )
+// })
 
 mongoose.connect(uri);
 
