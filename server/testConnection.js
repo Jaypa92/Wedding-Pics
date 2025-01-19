4b1351469d68121f6f83ef5da4bb92b9728f2268
[Jaypa92] <j.parris1@aol.com>
2025-01-19 17:49:27 -0500

    commit

--- server/testConnection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const uri = 'mongodb+srv://besteverwas92:Iamjaypa92@cluster0.xkq9il7.mongodb.net/Wedding?retryWrites=true&w=majority&appName=Cluster0';
-
-mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB!'))
-  .catch((err) => console.error('Error connecting to MongoDB:', err));
